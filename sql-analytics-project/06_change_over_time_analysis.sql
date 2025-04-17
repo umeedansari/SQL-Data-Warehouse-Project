b91c3295edc505be012a188bdc1246a3dc344b7c
umeedansari <aumeed179@gmail.com>
2025-04-17 18:48:41 +0530

    Create 06_change_over_time_analysis.sql

---
+++ sql-analytics-project/06_change_over_time_analysis.sql
@@ -0,0 +1,47 @@
+/*
+===============================================================================
+Change Over Time Analysis
+===============================================================================
+Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: EXTRACT(), DATET_RUNC(), TO_CHAR()
+    - Aggregate Functions: SUM(), COUNT(), AVG()
+===============================================================================
+*/
+-- Using EXTRACT for Year and Month
+SELECT
+    EXTRACT(YEAR FROM order_date) AS order_year,
+    EXTRACT(MONTH FROM order_date) AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY EXTRACT(YEAR FROM order_date), EXTRACT(MONTH FROM order_date)
+ORDER BY EXTRACT(YEAR FROM order_date), EXTRACT(MONTH FROM order_date);
+
+-- Using DATE_TRUNC for Month
+SELECT
+    DATE_TRUNC('month', order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATE_TRUNC('month', order_date)
+ORDER BY DATE_TRUNC('month', order_date);
+
+-- Using TO_CHAR for Date Formatting
+SELECT
+    TO_CHAR(order_date, 'YYYY-Mon') AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY TO_CHAR(order_date, 'YYYY-Mon')
+ORDER BY TO_CHAR(order_date, 'YYYY-Mon');
